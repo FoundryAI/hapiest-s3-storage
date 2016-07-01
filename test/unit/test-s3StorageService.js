70f1d42dafc6cb23d8a2e447a2607a2ecbed38dd
jonathan-fulton <jonathan@videoblocks.com>
2016-06-30 20:36:06 -0400

    Added keyPrefix option that allows S3StorageService to only have permissions to read/write from within a certain "directory"

--- test/unit/test-s3StorageService.js
+++ test/unit/test-s3StorageService.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Should = require('should');
+const Promise = require('bluebird');
 const Path = require('path');
 const Fs = require('fs');
 const rmdir = require('rmdir');
@@ -32,9 +33,29 @@ const s3ServiceLocalStorageWithoutBucket = S3StorageServiceFactory.create({
     }
 }, logger, basePath);
 
+const s3ServiceLocalStorageWithBucketAndKeyPrefix = S3StorageServiceFactory.create({
+    type: 'localstorage',
+    bucket: testBucket1,
+    keyPrefix: 'tmp/blah/hello',
+    localConfig: {
+        path: './'
+    }
+}, logger, basePath);
+
 const s3ServiceWithBucket = S3StorageServiceFactory.create({
     type: 's3',
     bucket: 'vizualai-test',
+    s3Config: {
+        userName: NodeConfig.get('aws.userName'),
+        awsAccessKey: NodeConfig.get('aws.awsAccessKey'),
+        awsSecretKey: NodeConfig.get('aws.awsSecretKey')
+    }
+});
+
+const s3ServiceWithBucketAndKeyPrefix = S3StorageServiceFactory.create({
+    type: 's3',
+    bucket: 'vizualai-test',
+    keyPrefix: 'images/tmp',
     s3Config: {
         userName: NodeConfig.get('aws.userName'),
         awsAccessKey: NodeConfig.get('aws.awsAccessKey'),
@@ -59,78 +80,100 @@ describe('S3StorageService', function() {
         it('Should put an object and then get the object with the same key', function () {
             const testKey = 'some/key/up/there.txt';
             return s3ServiceLocalStorageWithBucket.putObject({
-                    Key: testKey,
-                    Body: testFileBody
-                })
-                .then((results) => {
-                    (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.True();
-                    return s3ServiceLocalStorageWithBucket.getObject({
-                        Key: testKey
-                    });
-                })
-                .then(result => {
-                    (result.Key).should.eql(testKey);
-                    (result.Body).should.eql(testFileBody);
-                })
+                Key: testKey,
+                Body: testFileBody
+            })
+            .then((results) => {
+                (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.True();
+                return s3ServiceLocalStorageWithBucket.getObject({
+                    Key: testKey
+                });
+            })
+            .then(result => {
+                (result.Key).should.eql(testKey);
+                (result.Body).should.eql(testFileBody);
+            });
         });
 
         it('Should put an object and then get the object with the same key by providing the bucket in individual functions', function () {
             const testKey = 'blah.txt';
             return s3ServiceLocalStorageWithoutBucket.putObject({
+                Bucket: testBucket2,
+                Key: testKey,
+                Body: testFileBody
+            })
+            .then((results) => {
+                (Fs.existsSync(Path.join(basePath, testBucket2, testKey))).should.be.True();
+                return s3ServiceLocalStorageWithoutBucket.getObject({
                     Bucket: testBucket2,
-                    Key: testKey,
-                    Body: testFileBody
-                })
-                .then((results) => {
-                    (Fs.existsSync(Path.join(basePath, testBucket2, testKey))).should.be.True();
-                    return s3ServiceLocalStorageWithoutBucket.getObject({
-                        Bucket: testBucket2,
-                        Key: testKey
-                    });
-                })
-                .then(result => {
-                    (result.Key).should.eql(testKey);
-                    (result.Body).should.eql(testFileBody);
-                })
+                    Key: testKey
+                });
+            })
+            .then(result => {
+                (result.Key).should.eql(testKey);
+                (result.Body).should.eql(testFileBody);
+            });
         });
         
         it('Should delete an object', function () {
             const testKey = 'some/key/up/there.txt';
             return s3ServiceLocalStorageWithBucket.putObject({
-                    Key: testKey,
-                    Body: testFileBody
-                })
-                .then((results) => {
-                    (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.True();
-                })
-                .then(() => s3ServiceLocalStorageWithBucket.deleteObject({Key: testKey}))
-                .then(() => s3ServiceLocalStorageWithBucket.getObject({Key: testKey}))
-                .then(() => {
-                    throw new Error('Should fail with no such file error (err.code === ENOENT)')
-                }, err => {
-                    Should.exist(err);
-                    err.code.should.eql('ENOENT')
-                })
+                Key: testKey,
+                Body: testFileBody
+            })
+            .then((results) => {
+                (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.True();
+            })
+            .then(() => s3ServiceLocalStorageWithBucket.deleteObject({Key: testKey}))
+            .then(() => s3ServiceLocalStorageWithBucket.getObject({Key: testKey}))
+            .then(() => {
+                throw new Error('Should fail with no such file error (err.code === ENOENT)')
+            }, err => {
+                Should.exist(err);
+                err.code.should.eql('ENOENT')
+            });
         });
 
         it('Should upload an object and then get the object with the same key', function () {
             var testKey = 'myUploadKey';
             s3ServiceLocalStorageWithBucket.upload({
+                Key: testKey,
+                Bucket: testBucket1,
+                Body: testFileBody
+            }, {})
+            .then(function (result) {
+                (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.equal(true);
+                return s3ServiceLocalStorageWithBucket.getObject({
                     Key: testKey,
-                    Bucket: testBucket1,
+                    Bucket: testBucket1
+                })
+            })
+            .then(function (result) {
+                (result.Key).should.eql(testKey);
+                (result.Body).should.eql(testFileBody);
+            });
+        });
+
+        it('Should put an object with "short" key and get the object with both the "short" and full key', function() {
+            var testKey = 'myPutKey';
+            return s3ServiceLocalStorageWithBucketAndKeyPrefix.putObject({
+                    Key: testKey,
                     Body: testFileBody
-                }, {})
-                .then(function (result) {
-                    (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.equal(true);
-                    return s3ServiceLocalStorageWithBucket.getObject({
-                        Key: testKey,
-                        Bucket: testBucket1
-                    })
                 })
-                .then(function (result) {
-                    (result.Key).should.eql(testKey);
-                    (result.Body).should.eql(testFileBody);
+                .then(results => {
+                    return Promise.all([
+                        s3ServiceLocalStorageWithBucketAndKeyPrefix.getObject({Key: testKey}),
+                        s3ServiceLocalStorageWithBucketAndKeyPrefix.getObject({Key: `tmp/blah/hello/${testKey}`})
+                    ]);
                 })
+                .spread((resultShortKey, resultLongKey) => {
+                    resultShortKey.Key.should.eql(`tmp/blah/hello/${testKey}`);
+                    resultShortKey.Body.should.eql(testFileBody);
+
+                    resultLongKey.Key.should.eql(`tmp/blah/hello/${testKey}`);
+                    resultLongKey.Body.should.eql(testFileBody);
+                })
+                ;
         });
 
         
@@ -143,61 +186,83 @@ describe('S3StorageService', function() {
         it('Should put an object and then get the object with the same key', function () {
             const testKey = 'some/key/up/there.txt';
             return s3ServiceWithBucket.putObject({
-                    Key: testKey,
-                    Body: testFileBody
-                })
-                .then((results) => {
-                    return s3ServiceWithBucket.getObject({
-                        Key: testKey
-                    });
-                })
-                .then(result => {
-                    (result.Key).should.eql(testKey);
-                    (result.Body).should.eql(testFileBody);
-                })
+                Key: testKey,
+                Body: testFileBody
+            })
+            .then((results) => {
+                return s3ServiceWithBucket.getObject({
+                    Key: testKey
+                });
+            })
+            .then(result => {
+                (result.Key).should.eql(testKey);
+                (result.Body).should.eql(testFileBody);
+            });
         });
 
         it('Should delete an object', function () {
             const testKey = 'some/key/up/there.txt';
             return s3ServiceWithBucket.putObject({
-                    Key: testKey,
-                    Body: testFileBody
-                })
-                .then(() => s3ServiceWithBucket.getObject({Key: testKey}))
-                .then(result => {
-                    (result.Key).should.eql(testKey);
-                    (result.Body).should.eql(testFileBody);
-                })
-                .then(() => s3ServiceWithBucket.deleteObject({Key: testKey}))
-                .then(() => s3ServiceWithBucket.getObject({Key: testKey}))
-                .then(() => {
-                    throw new Error('Should fail with no such file error (err.code === NoSuchKey)')
-                }, err => {
-                    Should.exist(err);
-                    err.code.should.eql('NoSuchKey');
-                })
+                Key: testKey,
+                Body: testFileBody
+            })
+            .then(() => s3ServiceWithBucket.getObject({Key: testKey}))
+            .then(result => {
+                (result.Key).should.eql(testKey);
+                (result.Body).should.eql(testFileBody);
+            })
+            .then(() => s3ServiceWithBucket.deleteObject({Key: testKey}))
+            .then(() => s3ServiceWithBucket.getObject({Key: testKey}))
+            .then(() => {
+                throw new Error('Should fail with no such file error (err.code === NoSuchKey)')
+            }, err => {
+                Should.exist(err);
+                err.code.should.eql('NoSuchKey');
+            });
         });
 
         it('Should upload an object and then get the object with the same key', function () {
             var testKey = 'myUploadKey';
             s3ServiceWithBucket.upload({
+                Key: testKey,
+                Bucket: testBucket1,
+                Body: testFileBody
+            })
+            .then(result =>{
+                (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.equal(true);
+                return s3ServiceWithBucket.getObject({
                     Key: testKey,
-                    Bucket: testBucket1,
+                    Bucket: testBucket1
+                })
+            })
+            .then(result => {
+                (result.Key).should.eql(testKey);
+                (result.Body).should.eql(testFileBody);
+            })
+        });
+
+        it('Should put an object with "short" key and get the object with both the "short" and full key', function() {
+            var testKey = 'myPutKey';
+            return s3ServiceWithBucketAndKeyPrefix.putObject({
+                    Key: testKey,
                     Body: testFileBody
                 })
-                .then(function (result) {
-                    (Fs.existsSync(Path.join(basePath, testBucket1, testKey))).should.be.equal(true);
-                    return s3ServiceWithBucket.getObject({
-                        Key: testKey,
-                        Bucket: testBucket1
-                    })
+                .then(results => {
+                    return Promise.all([
+                        s3ServiceWithBucketAndKeyPrefix.getObject({Key: testKey}),
+                        s3ServiceWithBucketAndKeyPrefix.getObject({Key: `images/tmp/${testKey}`})
+                    ]);
                 })
-                .then(function (result) {
-                    (result.Key).should.eql(testKey);
-                    (result.Body).should.eql(testFileBody);
+                .spread((resultShortKey, resultLongKey) => {
+                    resultShortKey.Key.should.eql(`images/tmp/${testKey}`);
+                    resultShortKey.Body.should.eql(testFileBody);
+
+                    resultLongKey.Key.should.eql(`images/tmp/${testKey}`);
+                    resultLongKey.Body.should.eql(testFileBody);
                 })
+                ;
         });
         
-    })
+    });
     
 });
